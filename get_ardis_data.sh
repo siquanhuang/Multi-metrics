17a58064565ea85b1da31b99f84948d6b9adcf17
william <liyijiang3000@gmail.com>
2025-06-16 19:15:13 -0700

    upload website

--- get_ardis_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/ardisdataset/ARDIS/master/ARDIS_DATASET_IV.rar
-# mkdir data/ARDIS
-unrar e ARDIS_DATASET_IV.rar data/ARDIS/
-rm -rf ARDIS_DATASET_IV.rar
